cf458ebb143f3c8ef42bda6152628499963829ea
Youssef oularbi <youssefoularbi628@gmail.com>
2026-06-04 21:24:00 +0100

    side bar (magsinier)

--- GesAchats.WPF/ViewModels/Magasinier/MagasinierShellViewModel.cs
+++ GesAchats.WPF/ViewModels/Magasinier/MagasinierShellViewModel.cs
@@ -2,14 +2,18 @@ using System.Windows.Input;
 using GesAchats.Core.Interfaces;
 using GesAchats.WPF.Services;
 using GesAchats.WPF.ViewModels.Base;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace GesAchats.WPF.ViewModels.Magasinier;
 
 public class MagasinierShellViewModel : BaseViewModel
 {
     private readonly IUserSession _userSession;
+    private readonly IServiceProvider _serviceProvider;
     private readonly INavigationService _navigationService;
     private string _userName = string.Empty;
+    private string _activePage = "Dashboard";
+    private bool _isCollapsed;
 
     public string UserName
     {
@@ -17,25 +21,44 @@ public class MagasinierShellViewModel : BaseViewModel
         set => SetProperty(ref _userName, value);
     }
 
+    public string ActivePage
+    {
+        get => _activePage;
+        set => SetProperty(ref _activePage, value);
+    }
+
+    public bool IsCollapsed
+    {
+        get => _isCollapsed;
+        set => SetProperty(ref _isCollapsed, value);
+    }
+
     public ICommand NavigateCommand { get; }
     public ICommand LogoutCommand { get; }
+    public ICommand ToggleSidebarCommand { get; }
 
-    public MagasinierShellViewModel(IUserSession userSession, INavigationService navigationService)
+    public MagasinierShellViewModel(IUserSession userSession, IServiceProvider serviceProvider, INavigationService navigationService)
     {
         _userSession = userSession;
+        _serviceProvider = serviceProvider;
         _navigationService = navigationService;
         UserName = _userSession.CurrentUser?.FullName ?? "Magasinier";
         Title = "GesAchats - Espace Magasinier";
 
-        NavigateCommand = new RelayCommand(p => _navigationService.NavigateTo(p?.ToString() ?? "Dashboard"));
+        NavigateCommand = new RelayCommand(p =>
+        {
+            var page = p?.ToString() ?? "Dashboard";
+            ActivePage = page;
+            _navigationService.NavigateTo(page);
+        });
         LogoutCommand = new RelayCommand(_ => ExecuteLogout());
+        ToggleSidebarCommand = new RelayCommand(_ => IsCollapsed = !IsCollapsed);
     }
 
     private void ExecuteLogout()
     {
         _userSession.EndSession();
-        var serviceProvider = ((App)System.Windows.Application.Current).ServiceProvider;
-        var loginWindow = Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.GetRequiredService<Views.Auth.LoginWindow>(serviceProvider);
+        var loginWindow = _serviceProvider.GetRequiredService<Views.Auth.LoginWindow>();
         loginWindow.Show();
 
         System.Windows.Application.Current.MainWindow?.Close();
